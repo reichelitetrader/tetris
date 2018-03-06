e09d456b6e9ae854158ada0667961ab5839f4482
Toshiba <Toshiba@192.168.1.4>
2018-03-06 20:29:44 +0100

    fix problem with hiden field

--- tetris/src/blocks/Dot.java
+++ tetris/src/blocks/Dot.java
@@ -1,30 +1,35 @@
 package blocks;
+
 import java.awt.Point;
 
 public class Dot {
 
-   public Point[] points = {new Point(4,0)};//new Point[1]; 
-        
-   public Point[] moveDown(){
-       for(int i=0;i<points.length;i++){
-           points[i].y++;
-       }
-       return points;
-   }
-   
-   public Point[] moveLeft(){
-       for(int i=0;i<points.length;i++){
-           points[i].x--;
-       }
-       return points;
-   }
-   
-   public Point[] moveRight(){
-       for(int i=0;i<points.length;i++){
-           points[i].x++;
-       }
-       return points;
-   }
+    public Point[] points = null;
+
+    public Dot() {
+        this.points = new Point[1];
+        this.points[0] = new Point(4, 0);
+
+    }
+
+    public Point[] moveDown() {
+        for (int i = 0; i < points.length; i++) {
+            points[i].y++;
+        }
+        return points;
+    }
+
+    public Point[] moveLeft() {
+        for (int i = 0; i < points.length; i++) {
+            points[i].x--;
+        }
+        return points;
+    }
+
+    public Point[] moveRight() {
+        for (int i = 0; i < points.length; i++) {
+            points[i].x++;
+        }
+        return points;
+    }
 }
-
-
--- tetris/src/blocks/Square.java
+++ tetris/src/blocks/Square.java
@@ -2,31 +2,17 @@ package blocks;
 
 import java.awt.Point;
 
-public class Square {
+public class Square extends Dot {
   
-    public Point[] points = {new Point(4,0),new Point(5,0), new Point(4,1), new Point(5,1)};
+    public Point[] squarePoints = {
+        new Point(4,0),
+        new Point(5,0),
+        new Point(4,1), 
+        new Point(5,1)
+    };
     
-    public Point[] moveDown(){
-       for(int i=0;i<points.length;i++){
-           points[i].y++;
-       }
-       return points;
+   public Square(){
+       this.points = squarePoints; 
    }
-   
-   public Point[] moveLeft(){
-       for(int i=0;i<points.length;i++){
-           points[i].x--;
-       }
-       return points;
-   }
-   
-   public Point[] moveRight(){
-       for(int i=0;i<points.length;i++){
-           points[i].x++;
-       }
-       return points;
-   }
-    
-    
     
 }
