ea9eca7868293375809e38baa9f9593bd67b1330
Toshiba <Toshiba@192.168.1.4>
2018-05-08 20:45:59 +0200

    rotate figure Pyramid works

--- tetris/src/blocks/Pyramid.java
+++ tetris/src/blocks/Pyramid.java
@@ -20,15 +20,15 @@ public class Pyramid extends Block {
          Point source = points[3];
         
         if(Orientation.TOP == orientation){
-            points[0].x = source.x+1;
+            points[0].x = source.x;
             points[1].x = source.x;
-            points[2].x = source.x;
-            points[3].x = source.x+1;
+            points[2].x = source.x+1;
+            points[3].x = source.x;
 
-            points[0].y = source.y;
-            points[1].y = source.y+1;
+            points[0].y = source.y-1;
+            points[1].y = source.y;
             points[2].y = source.y;
-            points[3].y = source.y;
+            points[3].y = source.y+1;
             orientation = Orientation.RIGHT;
         }else if(Orientation.RIGHT == orientation){
             points[0].x = source.x-1;
