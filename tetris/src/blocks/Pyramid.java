aae2fa63ebcbb1e97aaa5adaf2b0c025c0a0ba05
Toshiba <Toshiba@192.168.1.4>
2018-05-08 20:40:51 +0200

    rotate figure Pyramid works

--- tetris/src/blocks/Pyramid.java
+++ tetris/src/blocks/Pyramid.java
@@ -20,16 +20,16 @@ public class Pyramid extends Block {
          Point source = points[3];
         
         if(Orientation.TOP == orientation){
-//            points[0].x = source.x+1;
-//            points[1].x = source.x;
-//            points[2].x = source.x;
-//            points[3].x = source.x+1;
-//
-//            points[0].y = source.y;
-//            points[1].y = source.y+1;
-//            points[2].y = source.y;
-//            points[3].y = source.y;
-//            orientation = Orientation.RIGHT;
+            points[0].x = source.x+1;
+            points[1].x = source.x;
+            points[2].x = source.x;
+            points[3].x = source.x+1;
+
+            points[0].y = source.y;
+            points[1].y = source.y+1;
+            points[2].y = source.y;
+            points[3].y = source.y;
+            orientation = Orientation.RIGHT;
         }else if(Orientation.RIGHT == orientation){
             points[0].x = source.x-1;
             points[1].x = source.x;
@@ -41,19 +41,19 @@ public class Pyramid extends Block {
             points[2].y = source.y+1;
             points[3].y = source.y;
        orientation = Orientation.BOTTOM;
-//        }else if(Orientation.BOTTOM == orientation){
-//            points[0].x = source.x;
-//            points[1].x = source.x;
-//            points[2].x = source.x;
-//            points[3].x = source.x;
-//
-//            points[0].y = source.y;
-//            points[1].y = source.y;
-//            points[2].y = source.y;
-//            points[3].y = source.y;
-//       orientation = Orientation.LEFT;
-//        
-//        
+        }else if(Orientation.BOTTOM == orientation){
+            points[0].x = source.x;
+            points[1].x = source.x-1;
+            points[2].x = source.x;
+            points[3].x = source.x;
+
+            points[0].y = source.y-1;
+            points[1].y = source.y;
+            points[2].y = source.y;
+            points[3].y = source.y+1;
+       orientation = Orientation.LEFT;
+        
+        
 //    }else if(Orientation.LEFT == orientation){
 //            points[0].x = source.x;
 //            points[1].x = source.x;
