00fac730b192b565d90a69f02cfa1f3f2cfc2fda
Toshiba <Toshiba@192.168.1.4>
2018-10-09 20:27:53 +0200

    add buttons panel

--- tetris/src/tetris/Window.java
+++ tetris/src/tetris/Window.java
@@ -8,18 +8,28 @@ import javax.swing.JPanel;
 public class Window extends JFrame {
 
     private Board board = null;
-    JButton[][] buttons = new JButton[10][10];
+    JButton[][] buttons = new JButton[10][10]; 
 
     public Window(Board board) {
         this.board = board;
         final JPanel buttonsPanel = new JPanel();
         JFrame frame = this;
 
-        frame.getContentPane().add(buttonsPanel);
-
+        for(int i =0;i<buttons.length; i++){
+            for(int j=0;j<buttons[i].length;j++){
+                buttons[i][j] = new JButton(" ");
+                buttons[i][j].setText(" ");
+                buttons[i][j].setSize(400, 600);
+                buttonsPanel.add(buttons[i][j]);
+            }
+        }
+        
+        frame.getContentPane().add(buttonsPanel); //dodanie panelu do ramki
         frame.setVisible(true);
         frame.pack(); //spakowanie okna 
         frame.setSize(400, 600);
 
+        
+        
     }
 }
