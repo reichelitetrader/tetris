29096d219cb257ec496968686291d0b26714e009
Toshiba <Toshiba@192.168.1.4>
2018-10-09 20:56:17 +0200

    add actionlistener

--- tetris/src/tetris/Window.java
+++ tetris/src/tetris/Window.java
@@ -2,37 +2,47 @@ package tetris;
 
 import boards.Board;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Window extends JFrame {
-    
+
     private Board board = null;
-    JButton[][] buttons = new JButton[10][10];    
-    
+    JButton[][] buttons = new JButton[10][10];
+
     public Window(Board board) {
         this.board = board;
         final JPanel buttonsPanel = new JPanel();
         JFrame frame = this;
-        
+
         GridLayout Layout = new GridLayout(0, 1);
         this.setLayout(Layout);
         buttonsPanel.setLayout(new GridLayout(10, 10));
-        
+
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons[i].length; j++) {
                 buttons[i][j] = new JButton(" ");
                 buttons[i][j].setText(" ");
                 buttons[i][j].setSize(400, 600);
                 buttonsPanel.add(buttons[i][j]);
+                buttons[i][j].addActionListener(new ActionListener() {
+
+                    public void actionPerformed(ActionEvent e) {
+                        JOptionPane.showMessageDialog(null, "start");
+                    }
+                });
+
             }
         }
-        
+
         frame.getContentPane().add(buttonsPanel); //dodanie panelu do ramki
         frame.setVisible(true);
         frame.pack(); //spakowanie okna 
         frame.setSize(400, 600);
-        
+
     }
 }
